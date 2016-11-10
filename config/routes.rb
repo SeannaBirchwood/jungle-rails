69195c9b488b29e3a1a62691baeb74188f91ebf3
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-11-10 21:53:00 +0000

    Have user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,25 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # resource :signup, only: [:show] do
+  #   put :add_user
+  # end
+
+  get '/signup' => 'users#new'
+  get '/users' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
